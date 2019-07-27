f2bc0738212cd772215141a27337b99600d3be8a
Daniel Gomez <dagv21@live.com>
2019-07-27 16:56:40 -0500

    Package updated

---
+++ scripts/loadcells_acquisition.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import serial
+import time
+import os
+import random
+import rospy
+from std_msgs.msg import Float64
+
+class LoadCellSensor(object):
+    def __init__(self):
+        '''Parameters Inicialization '''
+        self.serial_port = rospy.get_param("port","/dev/ttyACM0")
+        os.system("sudo chmod 777 " + self.serial_port) #Enabling port permissions
+        self.port_parameters = { "br": rospy.get_param("baud_rate", 115200) }
+        self.ser = serial.Serial(self.serial_port, self.port_parameters["br"])
+        self.pub_frontal = rospy.Publisher("frontal_loadcell_data", Float64, queue_size = 1, latch = False)
+        self.pub_posterior = rospy.Publisher("posterior_loadcell1_data", Float64, queue_size = 1, latch = False)
+        ''' Node Configuration '''
+        rospy.init_node('loadcell_sensor', anonymous = True)
+        self.fs = 200
+
+    def read_data(self):
+        voltage1 = 0
+        voltage2 = 0
+        try:
+            data = self.ser.readline()
+            voltage1 = float(data[0:6])
+            voltage2 = float(data[8:16])
+            #print(voltage1,voltage2)
+        except:
+            rospy.logwarn("Data not received")
+        
+        return (voltage1,voltage2)
+
+
+def main():
+    loadcell_sensor = LoadCellSensor()
+    rate = rospy.Rate(loadcell_sensor.fs)
+    rospy.loginfo('Mining Data')
+    while not rospy.is_shutdown():
+        voltage1,voltage2 = loadcell_sensor.read_data()
+        loadcell_sensor.pub_posterior.publish(voltage1)
+        loadcell_sensor.pub_frontal.publish(voltage2)
+        rate.sleep()
+    loadcell_sensor.ser.close()
+    rospy.loginfo('Closed Port')
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        os.system('clear')
+        print("Program finished\n")
+        print e
+        raise
---
+++ scripts/signals.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import rospy
+import time
+import os
+import math
+import numpy
+import scipy.signal
+from dynamixel_workbench_msgs.msg import DynamixelStatusList
+from tflex_test_bench.msg import IMUData
+from geometry_msgs.msg import Vector3
+from std_msgs.msg import Int16, Float64
+from sensor_msgs.msg import Imu
+
+class Controller(object):
+    def __init__(self):
+        rospy.init_node('tflex_test_bench_topics_synchronization', anonymous = True)
+        self.pub_cmd_motor1 = rospy.Publisher("/tilt1_controller/command", Float64, queue_size = 1, latch = False)
+        self.pub_cmd_motor2 = rospy.Publisher("/tilt2_controller/command", Float64, queue_size = 1, latch = False)
+        self.pub_singal_norm_motor1 = rospy.Publisher("/goal_position_motor1", Int16, queue_size = 1, latch = False)  
+        self.t = [i*0.01 for i in range(10000)]
+        self.sin = []
+        for i in range(len(self.t)):
+            self.sin.append(0.35*math.pi*math.sin(0.5*2*math.pi*self.t[i]))
+        self.i = 0
+        self.unit_rad = 2*math.pi/4095
+        ''' Chirp Signal '''
+        f0 = 0
+        f1 = 30
+        t1 = 50
+        self.chirp_signal = scipy.signal.chirp(self.t, f0, t1, f1, method='linear', phi=0, vertex_zero=True)
+
+    def sin_signal(self):
+        self.pub_cmd_motor1.publish(self.sin[self.i])
+        self.pub_singal_norm_motor1.publish(552*self.sin[self.i] + 3295)
+        self.i+=1
+        if self.i == len(self.sin):
+            self.i = 0
+
+    def step_signal(self):
+        f = 0.8 
+        #max_range = 0.3068
+        max_range = 0.9
+        self.pub_cmd_motor1.publish(max_range)
+        self.pub_cmd_motor2.publish(0)
+        time.sleep(1/f) # Period
+        self.pub_cmd_motor1.publish(0)
+        self.pub_cmd_motor2.publish(-max_range)
+        time.sleep(1/f) # Period
+        
+    def chirp_publisher(self):
+        self.pub_cmd_motor1.publish(self.chirp_signal[self.i])
+        print(self.chirp_signal[self.i])
+        self.i+=1
+        if self.i == len(self.chirp_signal):
+            self.i = 0
+        
+
+def main():
+
+    c = Controller()
+    rate = rospy.Rate(100)
+    time.sleep(1)
+    i = 0
+    while not (rospy.is_shutdown()):
+        #c.sin_signal()
+        #c.step_signal()
+        c.chirp_publisher()
+        if c.i == 0:
+            break
+        #i= i+1
+        #if (i == 1):
+            #c.pub_cmd_motor1.publish(0)
+            #c.pub_cmd_motor2.publish(1)
+            #break;
+    rospy.loginfo("Synchronization Finished")
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print (e)
