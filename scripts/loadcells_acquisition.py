87ec5b72b2edcbde750861533805379937197eab
Daniel Gomez <dagv21@live.com>
2019-09-20 11:15:12 -0500

    Package Updated

--- scripts/loadcells_acquisition.py
+++ scripts/loadcells_acquisition.py
@@ -23,32 +23,37 @@ class LoadCellSensor(object):
         rospy.init_node('loadcell_sensor', anonymous = True)
         self.fs = 1000
         ''' Loadcell Configuration '''
-        self.m1 = -556.8687   #Posterior
+        self.m1 = 664.6   #Posterior
         self.m2 = 691.3322    #Frontal
+        self.vo1 = 0
+        self.vo2 = 0
 
     def initial_value(self):
         ''' Calculating Initial Values '''
         n = 0
         mean_value1 = []
         mean_value2 = []
-        while n<=10000:
+        while n<=100:
             value1,value2 = self.read_data();
             mean_value1 = numpy.append(mean_value1, value1)
             mean_value2 = numpy.append(mean_value2, value2)
             n += 1
-        self.vo1 = numpy.mean(mean_value1[-500:], axis=0)
-        self.vo2 = numpy.mean(mean_value2[-500:], axis=0)
-        rospy.loginfo("Initial Value Posterior: %s",self.vo1)
+        print(mean_value1[-50::])
+        self.vo1 = numpy.mean(mean_value1[-50::], axis=0, dtype=numpy.float64)
+        self.vo2 = numpy.mean(mean_value2[-100::], axis=0)
         rospy.loginfo("Initial Value Frontal: %s",self.vo2)
+        rospy.loginfo("Initial Value Posterior: %s",self.vo1)
 
     def read_data(self):
         value1 = 0
         value2 = 0
         try:
             data = self.ser.readline()
+            # print(data[19])
+            value2 = float(data[5:15])
             #value1 = float(data)
-            value1 = float(data[1:7])
-            value2 = float(data[8:15])
+            value1 = float(data[20:30])
+            #value2 = float(data[8:15])
         except:
             rospy.logwarn("Data not received")
         return (value1,value2)
@@ -66,11 +71,11 @@ def main():
     while not rospy.is_shutdown():
         value1,value2 = loadcell_sensor.read_data()
         force1 = loadcell_sensor.force(value1,loadcell_sensor.m1,loadcell_sensor.vo1);
-        force2 = loadcell_sensor.force(value2,loadcell_sensor.m2,loadcell_sensor.vo2)
+        #force2 = loadcell_sensor.force(value2,loadcell_sensor.m2,loadcell_sensor.vo2)
         loadcell_sensor.pub_posterior.publish(value1)
         loadcell_sensor.pub_frontal.publish(value2)
         loadcell_sensor.pub_posterior_force.publish(force1)
-        loadcell_sensor.pub_frontal_force.publish(force2)
+        #loadcell_sensor.pub_frontal_force.publish(force2)
         rate.sleep()
     loadcell_sensor.ser.close()
     rospy.loginfo('Closed Port')
